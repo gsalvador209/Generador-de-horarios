96be3b07d293f0118dfa917bd0e92807644a4aa7
Chava <dchavezvillanueva@gmail.com>
2025-01-12 23:21:53 -0600

    Se finalizó la UI con sus funcionalidades

--- visualizador.py
+++ visualizador.py
@@ -9,9 +9,10 @@ from bitarray import bitarray
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.resizable(False, False)
         self.title("Generador de Horarios")
         self.width = 600
-        self.height = 600
+        self.height = 650
         self.geometry(f"{self.width}x{self.height}")
         self.frames = {
             "Frame1": MenuClaves(self),
@@ -75,7 +76,7 @@ class MenuClaves(ttk.Frame):
             # El verdadero espacio donde se escriben las claves
             key_entry = ttk.Entry(key_frame, font=("Arial", 14), width=10, justify="center")
             key_entry.grid(row=0, column=0, padx=5)
-            
+            key_entry.focus_set()
             if existing_key != "":
                 #print("Existing key:", existing_key)
                 key_entry.insert(0, existing_key)
@@ -90,7 +91,6 @@ class MenuClaves(ttk.Frame):
         self.claves.remove(key_frame)
         key_frame.destroy()  # Remove from the UI
         
-        print(f"Se ha eliminado una clave. Actualmente hay {len(self.claves)} claves.")
         if len(self.claves) == self.max_claves-1:
             self.add_key_placeholder()
         self.update_keyframe_rows()
@@ -101,8 +101,7 @@ class MenuClaves(ttk.Frame):
         """
         key_entry = key_frame.winfo_children()[0]  # Obtiene el widget del texto
         key = key_entry.get()
-        if (len(key) == 4 or  len(key == 3)) and key.isdigit():
-            print(f"La clave {key} se ha registrado.")
+        if (len(key) == 4 or  len(key) == 3) and key.isdigit():
             # Bloquea la entrada en este entry
             key_entry.config(state="disabled")
             key_entry.unbind("<Return>")  
@@ -142,7 +141,7 @@ class MenuClaves(ttk.Frame):
         with open("claves.pkl", "wb") as file:
             pickle.dump(claves, file)
 
-        print("Las claves se guadradon exitosamente.")
+        #print("Las claves se guadradon exitosamente.")
         self.parent.show_frame("Frame2")
 
     def update_keyframe_rows(self):
@@ -195,37 +194,55 @@ class Indisponibility(ttk.Frame):
         self.parent = parent  
         self.OFFSET_X = 60
         self.OFFSET_Y = 30
-        self.WIDTH = parent.width + self.OFFSET_X
-        self.HEIGHT = parent.height + self.OFFSET_Y
+        self.button_frame_height = 35
+        self.text_frame_height = 30
+        self.WIDTH = parent.width - self.OFFSET_X
+        self.HEIGHT = parent.height - self.OFFSET_Y - self.button_frame_height - self.text_frame_height
         self.color = "LightGoldenrod3"
         self.days = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
         self.hours = [f"{h}:00" for h in range(7, 22)] + [f"{h}:30" for h in range(7, 22)]
         self.hours = sorted(self.hours, key=lambda t: (int(t.split(":")[0]), int(t.split(":")[1])))
+        
         # Dimensiones y datos iniciales
-        self.CELL_WIDTH = self.WIDTH // 8
-        self.CELL_HEIGHT = (self.HEIGHT-150) // 30
+        self.CELL_WIDTH = self.WIDTH // 6 # Solo 6 porque el offset incluye el espacio para los días
+        self.CELL_HEIGHT = (self.HEIGHT) // 30 # 30 bloques de 30 minutos
         self.state = ['waiting']
 
         # Crear Canvas
-        self.canvas = tk.Canvas(self, width=self.WIDTH, height=self.HEIGHT-100, bg="white")
+        text_frame = ttk.Frame(self, height=self.text_frame_height)
+        text_frame.pack(side="top", fill="x", expand=True)
+
+        self.label = ttk.Label(text_frame, text="Selecciona los bloques de tiempo en los que no estás disponible", font=("Arial", 12))
+        self.label.pack(side="top")
+
+        self.canvas = tk.Canvas(self, width=self.WIDTH, height=self.HEIGHT, bg="white")
+        self.canvas.pack(side="top", fill="both", expand=True)
+
         self._open_schedule() # Cargar horario guardado
         self._draw_saved_schedule()
-        self.canvas.pack()
-
+        
         self.canvas.bind("<B1-Motion>",self._on_drag)
         self.canvas.bind("<ButtonRelease-1>",self._released_togle)
         # Dibujar cuadrícula inicial
         self._draw_grid()
 
         # Botones
-        button_frame = tk.Frame(self)
-        button_frame.pack(side="bottom", fill="x")
+        button_frame = tk.Canvas(self)
+        button_frame.pack(expand=True, fill="both")
+        
+        button_grid = tk.Frame(button_frame)
+        button_grid.pack(expand=True, fill="both")
 
-        self.reset_button = ttk.Button(button_frame ,text="Limpiar hoja", command=self._reset_schedule)
-        self.reset_button.pack(side="left", padx=10, pady=10)
+        button_grid.grid_rowconfigure(0, weight=1)
+        button_grid.grid_rowconfigure(2, weight=1)
+        button_grid.grid_columnconfigure(0, weight=1)
+        button_grid.grid_columnconfigure(3, weight=1)
 
-        self.save_button = ttk.Button(button_frame, text="Generar", command=self._save_schedule)
-        self.save_button.pack(side="left", padx=10, pady=10)
+        reset_button = tk.Button(button_grid ,text="Limpiar hoja", command=self._reset_schedule)
+        reset_button.grid(row=1, column=1, padx= 20 ,pady=5)
+        
+        save_button = tk.Button(button_grid, text="Generar", command=self._save_schedule)
+        save_button.grid(row=1, column=2, padx=20 ,pady=5,sticky="nsew")
 
     def _open_schedule(self):
         """
@@ -287,8 +304,6 @@ class Indisponibility(ttk.Frame):
                     )
 
     def _draw_grid(self):
-        # Crear encabezado de texto
-        self.canvas.create_text(self.WIDTH // 2, self.OFFSET_Y // 2, text="Horario de Clases", font=("Arial", 16, "bold"))
         
         # Dibujar encabezados de días
         for i, day in enumerate(self.days):
@@ -307,10 +322,10 @@ class Indisponibility(ttk.Frame):
         # Dibujar líneas de cuadrícula
         for i in range(len(self.days) + 1):
             x = i * self.CELL_WIDTH + self.OFFSET_X
-            self.canvas.create_line(x, self.OFFSET_Y, x, self.HEIGHT, fill="gray")
+            self.canvas.create_line(x, self.OFFSET_Y, x, self.HEIGHT+self.OFFSET_Y, fill="gray")
         for i in range(len(self.hours) + 1):
             y = i * self.CELL_HEIGHT + self.OFFSET_Y
-            self.canvas.create_line(self.OFFSET_X, y, self.WIDTH, y, fill="gray")
+            self.canvas.create_line(self.OFFSET_X, y, self.WIDTH + self.OFFSET_X, y, fill="gray")
 
     def _released_togle(self,event):
         if self.state[0] != 'waiting':
